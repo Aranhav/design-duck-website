af6206226f049d797d385c9681ed59e943423344
Shubham Home <agilevisiondev@gmail.com>
2025-06-29 14:48:52 +0530

    Update project version to 0.1.0 and enhance Vite configuration with VitePluginRadar for analytics; also upgrade Node.js version in CI/CD workflow to 20.x and update GitHub Actions to use the latest versions.

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { VitePluginRadar } from "vite-plugin-radar";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePluginRadar({
+      analytics: {
+        id: "G-XD4550MV3V",
+      },
+    }),
+  ],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
